caf5fdd751b294339b95238f522493557780b313
smithc57973 <44327733+smithc57973@users.noreply.github.com>
2020-11-03 15:27:55 -0600

    Started challenge 4

---
+++ Challenge4/Assets/Challenge 4/Scripts/SpawnManagerX.cs
@@ -0,0 +1,107 @@
+﻿/*
+ * Chris Smith
+ * Challenge 4
+ * Script to spawn enemies
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+
+    private float spawnRangeX = 10;
+    private float spawnZMin = 15; // set min spawn Z
+    private float spawnZMax = 25; // set max spawn Z
+
+    public int enemyCount;
+    public int waveCount = 1;
+    public float enemySpeed;
+
+    public bool gameOver = false;
+    public bool gameWon = false;
+    public UIManager uiManager;
+
+    public GameObject player; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        enemySpeed = waveCount;
+
+        if (enemyCount == 0)
+        {
+            SpawnEnemyWave(waveCount);
+        }
+
+        if (waveCount == 12)
+        {
+            gameWon = true;
+            gameOver = true;
+        }
+
+        if (gameOver)
+        {
+            Time.timeScale = 0f;
+            if (gameWon)
+            {
+                uiManager.gameWin.enabled = true;
+            }
+            else
+            {
+                uiManager.gameLose.enabled = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+
+    }
+
+    // Generate random spawn position for powerups and enemy balls
+    Vector3 GenerateSpawnPosition ()
+    {
+        float xPos = Random.Range(-spawnRangeX, spawnRangeX);
+        float zPos = Random.Range(spawnZMin, spawnZMax);
+        return new Vector3(xPos, 0, zPos);
+    }
+
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        Vector3 powerupSpawnOffset = new Vector3(0, 0, -15); // make powerups spawn at player end
+
+        // If no powerups remain, spawn a powerup
+        if (GameObject.FindGameObjectsWithTag("Powerup").Length == 0) // check that there are zero powerups
+        {
+            Instantiate(powerupPrefab, GenerateSpawnPosition() + powerupSpawnOffset, powerupPrefab.transform.rotation);
+        }
+
+        // Spawn number of enemy balls based on wave number
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+
+        waveCount++;
+        ResetPlayerPosition(); // put player back at start
+
+    }
+
+    // Move player back to position in front of own goal
+    void ResetPlayerPosition ()
+    {
+        player.transform.position = new Vector3(0, 1, -7);
+        player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        player.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+    }
+
+}
---
+++ Challenge4/Assets/Challenge 4/Scripts/UIManager.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Chris Smith
+ * Challenge 4
+ * Script for controlling UI elements
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text waves;
+    public Text enemies;
+    public Text gameWin;
+    public Text gameLose;
+    public GameObject tutPanel;
+    public SpawnManagerX spawnManager;
+
+    void Awake()
+    {
+        gameWin.enabled = false;
+        gameLose.enabled = false;
+    }
+
+    private void Start()
+    {
+        Time.timeScale = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            tutPanel.gameObject.SetActive(false);
+            Time.timeScale = 1f;
+        }
+
+        waves.text = "Wave: " + (spawnManager.waveCount - 1);
+        enemies.text = "Enemies Left: " + spawnManager.enemyCount;
+    }
+}
+
+
+//To do
+//The enemies never get more difficult – The enemies’ speed should increase in speed by a small amount with every new wave 
+//    Hint - You’ll need to track and increase the enemy speed in SpawnManagerX.cs.Then in EnemyX.cs, reference that speed variable and set it in Start().
+
+//Add loss condition: all enemies in the current wave get through the player goal.
+
+    
